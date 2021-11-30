6f98314e6d4d04c99c5946a279ed8b41c06ae3e0
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2021-12-01 02:35:09 +0900

    Add video support

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,16 @@ use tracing::Span;
 
 mod cli;
 
+const IMAGE_EXTTENSIONS: &[&str] = &[
+    "apng", "avif", "gif", "jpg", "jpeg", "jfif", "pjpeg", "pjp", "png", "svg", "webp", "bmp",
+    "ico", "cur", "tif", "tiff",
+];
+
+const VIDEO_EXTTENSIONS: &[&str] = &[
+    "3gp", "mpg", "mpeg", "mp4", "m4v", "m4p", "ogv", "ogg", "mov", "webm", "aac", "flac", "mp3",
+    "m4a", "oga", "wav",
+];
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let opt = cli::Opt::init();
@@ -121,9 +131,43 @@ async fn handle_404(uri: Uri) -> Result<impl IntoResponse, AppError> {
 }
 
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
+enum MediaType {
+    Image,
+    Video,
+    Other,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 enum File {
-    Directory { path: String, name: String },
-    File { path: String, name: String },
+    Directory {
+        path: String,
+        name: String,
+    },
+    File {
+        path: String,
+        name: String,
+        media_type: MediaType,
+    },
+}
+
+impl MediaType {
+    pub fn new(path: &Path) -> Self {
+        match path
+            .extension()
+            .map(|e| e.to_string_lossy().to_string().to_ascii_lowercase())
+        {
+            Some(ext) => {
+                if IMAGE_EXTTENSIONS.contains(&ext.as_str()) {
+                    Self::Image
+                } else if VIDEO_EXTTENSIONS.contains(&ext.as_str()) {
+                    Self::Video
+                } else {
+                    Self::Other
+                }
+            }
+            None => Self::Other,
+        }
+    }
 }
 
 impl File {
@@ -142,10 +186,36 @@ impl File {
         let file = if path_buf.is_dir() {
             Self::Directory { path, name }
         } else {
-            Self::File { path, name }
+            Self::File {
+                path,
+                name,
+                media_type: MediaType::new(path_buf),
+            }
         };
 
         Ok(file)
+    }
+
+    pub fn is_image(&self) -> bool {
+        matches!(
+            self,
+            Self::File {
+                path: _,
+                name: _,
+                media_type: MediaType::Image,
+            }
+        )
+    }
+
+    pub fn is_video(&self) -> bool {
+        matches!(
+            self,
+            Self::File {
+                path: _,
+                name: _,
+                media_type: MediaType::Video,
+            }
+        )
     }
 }
 
